fb43c624bfd8dbad66f3245038a81851593203c2
sirmcclay <mcclay.design@gmail.com>
2020-09-05 17:26:37 -0300

    feat:Add dependency injections | CreateCustomerService

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findCustomerByEmail = this.customersRepository.findByEmail(email);
+
+    if (findCustomerByEmail) {
+      throw new AppError('Customer already exists.');
+    }
+
+    const customer = this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
