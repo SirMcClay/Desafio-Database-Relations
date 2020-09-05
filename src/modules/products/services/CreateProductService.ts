8f48fc3a52d10452450e11aeb08de82c29937e09
sirmcclay <mcclay.design@gmail.com>
2020-09-05 18:22:17 -0300

    feat:Products controller and service

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const findProductByName = await this.productsRepository.findByName(name);
+
+    if (findProductByName) {
+      throw new AppError('Product already exists.');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
