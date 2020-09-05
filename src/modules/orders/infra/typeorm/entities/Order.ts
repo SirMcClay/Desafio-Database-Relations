49ba34afe65cde9084c01e2b766002441166a85b
sirmcclay <mcclay.design@gmail.com>
2020-09-05 17:08:57 -0300

    feat:TypeORM entities config

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -6,20 +6,35 @@ import {
   ManyToOne,
   JoinColumn,
   OneToMany,
+  Column,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
+import { mainModule } from 'process';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  customer_id: string;
+
+  @ManyToMany(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  @ManyToMany(() => OrdersProducts)
+  @JoinTable()
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -4,24 +4,34 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
-  OneToMany,
+  JoinTable,
+  ManyToMany,
 } from 'typeorm';
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('product')
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   price: number;
 
+  @Column()
   quantity: number;
 
+  @ManyToMany(() => OrdersProducts)
+  @JoinTable()
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
